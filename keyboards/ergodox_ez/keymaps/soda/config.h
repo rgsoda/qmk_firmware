bcbef56061b968d53410f8cd70b7916242d5bdde
esoda <rafalgalczynski@gmail.com>
2020-09-25 11:33:12 +0200

    soda initial commit

---
+++ keyboards/ergodox_ez/keymaps/soda/config.h
@@ -0,0 +1,13 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef TAPPING_TERM
+#define TAPPING_TERM 258
+
+#define FIRMWARE_VERSION u8"Da4rq/QjBGp"
+#define RGB_MATRIX_STARTUP_SPD 60
+
+
+#define LEADER_TIMEOUT 800 // leader key sequence timeout in millis
